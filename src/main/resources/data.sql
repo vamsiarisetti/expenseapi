bed9eea1551563c7a9ea4ea39eb631fa08dbe3f9
vamsiarisetti <taurus.vamsi@gmail.com>
2024-10-02 16:45:13 +0530

    first commit

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO TRANSACTIONS (transaction_name, transaction_type, transaction_date, transaction_amount, transaction_month, transaction_category, transaction_year) VALUES
-    ('BZA Rent', 'income', '2024-10-05', 8000, 'October', 'Rental Income', 2024),
-    ('HYD Rent', 'income', '2024-10-10', 17000, 'October', 'Rental Income', 2024),
-    ('Rent', 'expense', '2024-10-02', 14000.00, 'October', 'Rent', 2024);
